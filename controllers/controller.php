bd030f78df05695a5c45b0982a7525f03ef2b92a
mru24 <mru24@yahoo.com>
2019-03-22 11:38:50 +0000

    Views Controllers create

--- controllers/controller.php
+++ controllers/controller.php
@@ -2,8 +2,9 @@
 
 class Controller {
 
-  public static function CreateView() {
-    echo "View Created";
+  public static function CreateView($view_name) {
+    require_once("./views/$view_name.php");
+
   }
 
 }
--- routes.php
+++ routes.php
@@ -1,13 +1,17 @@
 <?php
 
+Route::set('index.php', function() {
+  Index::CreateView('Index');
+});
+
 Route::set('home', function() {
-  Home::CreateView();
+  Home::CreateView('Home');
 });
 
 Route::set('about', function() {
-  About::CreateView();
+  About::CreateView('About');
 });
 
 Route::set('contact', function() {
-  Contact::CreateView();
+  Contact::CreateView('Contact');
 });
